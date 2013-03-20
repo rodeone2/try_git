32a4c4642c83ba50e7b0738f49d9e0ccfa9c9d2e
robin deatherage <about2mount@gmail.com>
2013-03-20 11:38:15 -0500

    config file test

---
+++ a1/config.php
@@ -0,0 +1,35 @@
+<?php
+//SITE WIDE NAMING AND CONFIGURATION//
+
+//META VALUES//
+
+//PAGE TITLES//
+
+//PAGE NAMES//
+
+//PAGE PATHS GROUPED BY MODULE//
+
+//PATHS PUBLIC(static)//
+//LISTED WITH "d" AS CONTENT SECTIONS THAT CHANGE//
+$v1a='v1/head.php';
+$v1b='v1/hz_menu.php';
+$v1c='v1/lft_menu.php';
+//Content Pages//
+$v1dh='v1/home.php';
+$v1da='v1/about.php';
+$v1df='v1/faq.php';
+$v1dg='v1/contact.php';
+$v1dg
+//Last Page Paths//
+$v1e='v1/rht_menu.php';
+$v1f='v1/add_menu.php';
+$v1g='v1/footer.php';
+
+//PATHS PROTECTED(members)//
+
+//PATHS PRIVATE(admin)//
+
+
+
+?>
+
